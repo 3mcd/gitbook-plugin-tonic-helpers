2e35871b3e106e78f3cd7e306521ef4997256fc6
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2016-01-06 14:00:13 -0500

    Removed blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,39 +5,21 @@ require(['gitbook'], function (gitbook) {
     return Array.prototype.slice.apply(arr, args);
   }
 
-  function init() {
-    gitbook.state.$book.find('[data-tonic-embed]').each(function () {
-      initEmbed(this);
-    });
+  function success() {
+    removeActiveClasses(el);
+    el.dataset.message = ['Success!'].concat(slice(arguments)).join(' ');
+    el.classList.add('is-success');
   }
 
-  function initEmbed(el) {
-    var results = [];
-
-    function success() {
-      removeActiveClasses(el);
-      el.dataset.message = ['Success!'].concat(slice(arguments)).join(' ');
-      el.classList.add('is-success');
-    }
-
-    function failure() {
-      removeActiveClasses(el);
-      el.dataset.message = ['Failure.'].concat(slice(arguments)).join(' ');
-      el.classList.add('is-failure');
-    }
-
-    Tonic.createNotebook({
-      element: el,
-      source: el.dataset.tonicSource,
-      onResult: function (res) {
-        var handler = new Function('result', 'results', 'success', 'failure', el.dataset.tonicResult).toString();
-        eval('(' + handler + ')(res, results, success, failure);');
-      }
-    });
+  function failure() {
+    removeActiveClasses(el);
+    el.dataset.message = ['Failure.'].concat(slice(arguments)).join(' ');
+    el.classList.add('is-failure');
   }
 
-  gitbook.events.bind('page.change', function () {
-    init();
-  });
+  gitbook.tonic = {
+    success: success,
+    failure: failure
+  };
 
 });
--- index.js
+++ index.js
@@ -1,46 +1,16 @@
 var _ = require('lodash');
 
-var tplWebsite = _.template(fs.readFileSync(path.resolve(__dirname, "./assets/templates/website.html")));
-var tplEbook = _.template(fs.readFileSync(path.resolve(__dirname, "./assets/templates/ebook.html")));
-
 module.exports = {
-
   website: {
-
     assets: './assets',
-
     css: [
       'css/TonicEmbed.css'
     ],
-
     js: [
       'js/main.js'
     ],
-
     html: {
       'body:end': function () { return '<script src="https://embed.tonicdev.com"></script>'; }
     }
-
-  },
-
-  blocks: {
-
-    tonic: {
-
-      blocks: ['source', 'result'],
-
-      process: function (block) {
-        var props = block.blocks.reduce(function (a, x) {
-          return (a[x.name] = x.body.trim(), a);
-        }, {});
-
-        var tpl = (this.generator === 'website' ? tplWebsite : tplEbook);
-
-        return tpl({ props: props });
-      }
-
-    }
-
   }
-
 };
